4853f968379faffa83c954deb9a0cf9360742d4a
khanaldeepak <khanaldeepak1988@gmail.com>
2015-10-10 17:28:03 -0400

    Initial Phase with Home Page

---
+++ index.php
@@ -0,0 +1,237 @@
+<?php require_once 'includes/header.php'; ?>
+
+<?php require_once 'includes/slider.php'; ?>
+<!--Main Body Contents Starts-->
+<div class="bodycont bd_rd_10">
+<!--About School Starts-->
+<div class="fullwith bd_rd_10">
+	<li class="pad_tb_1 marginlt-3 oneoffive graybg center br-tl">All About The<br/> School</li>
+	<li class="pad_tb_1 oneoffive graybg center">Why You Should<br/> Study With Us</li>
+	<li class="pad_tb_1 oneoffive graybg center">Education And<br/> Student Experience</li>
+	<li class="pad_tb_1 oneoffive graybg center">Alumni And Its<br/> Donors</li>
+	<li class="pad_tb_1 oneoffive graybg center br-tr">Latest School<br/> News & Events</li>
+</div>
+<!--About School Ends-->
+<!--Our Services Starts-->
+<div class="one_quater float_l margin-1">
+	<li class="marginb-5">
+		<a href="#">
+			<em class="profilehead">Prospective Students</em> <hr/>
+			<img class="borderbox" src="images/demo/future.png" alt="">
+		</a>
+	</li>
+	<li class="marginb-5">
+		<a href="#">
+			<em class="profilehead">Current Students</em> <hr/>
+			<img class="borderbox" src="images/demo/current.png" alt="">
+		</a>
+	</li>
+	<li class="marginb-5">
+		<a href="#">
+			<em class="profilehead">Alumni</em> <hr/>
+			<img class="borderbox" src="images/demo/previous.png" alt="">
+		</a>
+	</li>
+	<li class="marginb-5">
+		<a href="#">
+			<em class="profilehead">Teachers</em> <hr/>
+			<img class="borderbox" src="images/demo/staff.png" alt="">
+		</a>
+	</li>
+</div>
+<div class="one_half float_l margin-1">
+	<h3 class="latest">Latest News & Events</h3><hr/>
+	<li class="marginb-5 studentprofile">
+		<section class="quater float_l">
+		<a href="#">
+			<img class="middlebox" src="images/demo/deo.png" alt="">
+		</a>
+		</section>
+		<section class="threequater float_r">
+		<a href="#">PRINCIPAL'S MESSAGE</a>
+		<p>Dear Readers, <br/>
+			It is the 
+			intellectual out come for the scholarly interaction among a cohesive
+			 team of dynamic and managerial professionals and distinguished 
+			 academicians from different sectors. 
+		</p>
+		</section>
+		<div class="clear"></div>
+	</li>
+	
+		<li class="marginb-5 studentprofile">
+		<section class="quater float_l">
+		<a href="#">
+			<img class="middlebox" src="images/demo/deo.png" alt="">
+		</a>
+		</section>
+		<section class="threequater float_r">
+		<a href="#">CHAIRMAN'S MESSAGE</a>
+		<p>Dear Readers, <br/>
+			 It is the 
+			intellectual out come for the scholarly interaction among a cohesive
+			 team of dynamic and managerial professionals and distinguished 
+			 academicians from different sectors. 
+		</p>
+		</section>
+		<div class="clear"></div>
+	</li>
+	
+	<li class="marginb-5 studentprofile">
+		<section class="quater float_l">
+		<a href="#">
+			<img class="middlebox" src="images/demo/deep.png" alt="">
+		</a>
+		</section>
+		<section class="threequater float_r">
+		<a href="#">CHAIRMAN'S MESSAGE</a>
+		<p>Dear Readers, <br/>
+			 It is the 
+			intellectual out come for the scholarly interaction among a cohesive
+			 team of dynamic and managerial professionals and distinguished 
+			 academicians from different sectors. 
+		</p>
+		</section>
+		<div class="clear"></div>
+	</li>
+	
+	<li class="float_r"><a href="#">Click here to view all of the latest news and events »</a></li>
+	<div class="clear"></div>
+
+</div>
+<div class="one_quater float_l margin-1">
+	<h3 class="latest">Virtual Tour</h3><hr/>
+	<section class="virtual">
+	<img src="images/demo/video.gif"/>
+	</section>
+	<li><a href="#">View More Tour Videos Here</a></li>
+	
+	<h3 class="latest margintop-15">Quick Information</h3><hr/>
+	<li>
+		<img class="thirtythree float_l" src="images/demo/88.jpg"/>
+		<a href="#">
+		<div class="sixtyseven graybg gray-dktext bold center padding_tb_6 float_r">
+		MAKE AN APPLICATION
+		</div>
+		</a>
+		<div class="clear"></div>
+	</li>
+
+	<li class="margintb-4">
+		<img class="thirtythree float_l" src="images/demo/99.jpg"/>
+		<div class="sixtyseven graybg gray-dktext bold center padding_tb_6 float_r">
+		 ORDER A PROSPECTUS
+		</div>
+		<div class="clear"></div>
+	</li>
+
+	<li>
+		<img class="thirtythree float_l" src="images/demo/77.jpg"/>
+		<div class="sixtyseven graybg gray-dktext bold center padding_tb_6 float_r">
+		 GRADUATE PROGRAMS
+		</div>
+		<div class="clear"></div>
+	</li>
+	
+</div>
+<div class="clear"></div>
+<!--Our Services Ends-->
+<!--Twitter Section Starts-->
+<div class="nine-five bd_rd_10 navybg marginauto pad_tb-1">
+	<div class="one_quater borderrt-2 height150 float_l center">
+		<a class="twitter-text" href="#">
+			<i class="fa fa-twitter fa-3x twitter-text margintop-15"></i><br/>
+			Follow Us On Twitter
+		</a>
+	</div>
+	<div class="threequater pad_l_5 float_r">
+		<p class="margintb-4"><span class="white-text large">Inteligula congue id elis donec sce sagittis 
+		intes id laoreet aenean. Massawisi condisse leo sem ac tincidunt
+		nibh quis dui fauctor et donecnibh elis velit <a class="twitter-text" href="#">@name</a> - 10:15 AM yesterday</span>
+		</p>
+	</div>
+	<div class="clear"></div>
+</div>
+<!--Twitter Section Ends-->
+<?php require_once'includes/quicklinks.php'; ?>
+
+
+<!--What Our Clients Says Starts-->
+<div class="reviews navybg bd_rd_10 margintb-1">
+<h3 class="white-text">What Our Students Says About Us</h3>
+<div class="half float_l">
+<!--Client Review Single Row Starts-->
+	<div class="reviewrow">
+        <div class="clientsimage float_l">
+            <img src="images/deepak.jpg" alt="Dan">
+        </div>
+                
+        <div class="saying float_l">
+            <blockquote>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia et pariatur ipsam tempora officia ea iusto expedita, nulla, hic odit saepe repellat nesciunt dolorum, officiis laborum ad, aliquam. Quos, et.</p>                        
+            </blockquote>
+            <h5>Deepak Khanal</h5>
+            <p>Co-Founder, The Times of Everest</p>
+        </div>
+		<div class="clear"></div>
+    </div>
+<!--Client Review Single Row Ends-->
+
+<!--Client Review Single Row Starts-->
+	<div class="reviewrow">
+        <div class="clientsimage float_l">
+            <img src="images/deepak.jpg" alt="Dan">
+        </div>
+                
+        <div class="saying float_l">
+            <blockquote>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia et pariatur ipsam tempora officia ea iusto expedita, nulla, hic odit saepe repellat nesciunt dolorum, officiis laborum ad, aliquam. Quos, et.</p>                        
+            </blockquote>
+            <h5>Deepak Khanal</h5>
+            <p>Co-Founder, The Times of Everest</p>
+        </div>
+		<div class="clear"></div>
+    </div>
+<!--Client Review Single Row Ends-->
+</div>
+<div class="half float_l">
+	<!--Client Review Single Row Starts-->
+	<div class="reviewrow">
+        <div class="clientsimage float_l">
+            <img src="images/dan.jpg" alt="Dan">
+        </div>
+                
+        <div class="saying float_l">
+            <blockquote>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia et pariatur ipsam tempora officia ea iusto expedita, nulla, hic odit saepe repellat nesciunt dolorum, officiis laborum ad, aliquam. Quos, et.</p>                        
+            </blockquote>
+            <h5>Dan Cederholm</h5>
+            <p>Co-Founder, Dribbble</p>
+        </div>
+		<div class="clear"></div>
+    </div>
+<!--Client Review Single Row Ends-->
+	<!--Client Review Single Row Starts-->
+	<div class="reviewrow">
+        <div class="clientsimage float_l">
+            <img src="images/dan.jpg" alt="Dan">
+        </div>
+                
+        <div class="saying float_l">
+            <blockquote>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia et pariatur ipsam tempora officia ea iusto expedita, nulla, hic odit saepe repellat nesciunt dolorum, officiis laborum ad, aliquam. Quos, et.</p>                        
+            </blockquote>
+            <h5>Dan Cederholm</h5>
+            <p>Co-Founder, Dribbble</p>
+        </div>
+		<div class="clear"></div>
+    </div>
+<!--Client Review Single Row Ends-->
+</div>
+<div class="clear"></div>
+</div>
+<!--What Our Clients Says Ends-->
+</div>
+<!--Main Body Contents Ends-->
+
+<?php require_once 'includes/footer.php'; ?>
---
+++ overview.php
@@ -0,0 +1,25 @@
+<?php require_once 'includes/header.php'; ?>
+
+
+<!--Main Body Contents Starts-->
+<div class="bodycont bd_rd_10">
+<!--About School Starts-->
+<div class="about">
+	<h3>ABOUT US</h3>
+		Munal English Boarding High School, which was established in 1980 with 
+		profound aim of enhancing knowledge of the students through practical 
+		education, has already celebrated its silver jubilee in 2005. The school 
+		has been 'Deepakalizing' the society and the students with the bright
+		rays of education since its establishment with the motto 'Education is the light of life.
+
+
+</div>
+
+<!--About School Ends-->
+
+<?php require_once'includes/quicklinks.php'; ?>
+
+</div>
+<!--Main Body Contents Ends-->
+
+<?php require_once 'includes/footer.php'; ?>
